82de8e899c95fcb09f1dc743df16bbc19cf3e0e8
polothil <polothil.5@gmail.com>
2021-03-03 22:57:58 +0530

    End of module 2

--- module 2/index.js
+++ module 2/index.js
@@ -2,17 +2,15 @@ const express = require('express');
 const Datastore = require('nedb');
 
 const app = express();
-
 app.listen(3000, () => console.log('Listening at port 3000'));
-
 app.use(express.static('public'));
-
 app.use(express.json({ limit: '1mb' }));
 
 const database = new Datastore('database.db');
 database.loadDatabase();
 
 app.get('/api', (request, response) => {
+  // response.json({ test: 123 });
   database.find({}, (error, data) => {
     if (error) {
       response.end();
